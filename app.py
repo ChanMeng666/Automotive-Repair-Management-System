31d446dae080da5dda6a7531db47f458406e4b36
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2024-01-24 16:18:22 +1300

    Further refinement of the search function on the technician's page & reformatting of the code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask import render_template
 from flask import request
 from flask import redirect
@@ -21,30 +21,21 @@ def getCursor():
     dbconn = connection.cursor()
     return dbconn
 
-
-# 设置基础的路由("/")，返回base.html页面
 @app.route("/")
 def home():
     return render_template("base.html")
 
-
-# 设置currentjobs的路由，用以执行分页,并显示结果在currentjoblist.html页面
 @app.route("/currentjobs")
 def currentjobs():
     connection = getCursor()
     page = int(request.args.get('page', 1))
-
     selection = request.args.get('selection', '')
     search = request.args.get('search', '')
-
     per_page = 10
     offset = (page - 1) * per_page
-
-    # 获取当前工作列表（即未完成的工作）的总长度（即总行数）
     connection.execute("SELECT COUNT(*) FROM job WHERE completed=0;")
     total = connection.fetchone()[0]
 
-    #对应的字段名映射，防止SQL注入
     column_map = {
         "Job ID": "job.job_id",
         "Customer ID": "job.customer",
@@ -56,30 +47,34 @@ def currentjobs():
     column_name = column_map.get(selection)
 
     if selection and search and column_name:
-        # 参数化值以防止sql注入
         connection.execute(f"SELECT job.job_id,job.customer,customer.first_name,customer.family_name,job.job_date FROM job INNER JOIN customer ON job.customer = customer.customer_id WHERE {column_name} LIKE %s AND job.completed=0 LIMIT %s OFFSET %s;", (f'%{search}%', per_page, offset))
     else:
-        # 如果没有提供选中的字段或搜索内容，则返回所有记录
         connection.execute("SELECT job.job_id,job.customer,customer.first_name,customer.family_name,job.job_date FROM job INNER JOIN customer ON job.customer = customer.customer_id WHERE job.completed=0 LIMIT %s OFFSET %s;", (per_page, offset))
 
     jobList = connection.fetchall()
 
-    # 判断查询结果是否为空
     no_result = False
     if len(jobList) == 0:
         no_result = True
 
     pagination = Pagination(page=page, total=total, per_page=per_page, record_name='jobs')
-    # 向模板传递一个新的变量no_result
-    return render_template('currentjoblist.html', job_list=jobList, pagination=pagination, no_result=no_result)
 
+    job_parts_services = dict()
+    for job in jobList:
+        job_id = job[0]
+        connection.execute("SELECT part_id, qty FROM job_part WHERE job_id=%s;", (job_id,))
+        parts = connection.fetchall()
 
+        connection.execute("SELECT service_id, qty FROM job_service WHERE job_id=%s;", (job_id,))
+        services = connection.fetchall()
 
-# 设置technician的路由，重定向到"/currentjobs"路由
+        job_parts_services[job_id] = {
+            "parts": parts,
+            "services": services,
+        }
+
+    return render_template('currentjoblist.html', job_list=jobList, job_parts_services=job_parts_services, pagination=pagination, no_result=no_result, selection=selection, search=search)
+
 @app.route("/technician")
 def technician():
     return redirect("/currentjobs")
-
-
-
-
