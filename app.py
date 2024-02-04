5aaa9a2957aba2490f85f0175f39c95dc3e375ce
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2024-02-04 19:30:23 +1300

    Improve the search function of currentjoblist.html page

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import connect
 
 app = Flask(__name__)
 
-
 dbconn = None
 connection = None
 app.secret_key = 'some_secret'
@@ -25,33 +24,19 @@ def getCursor():
         database=connect.dbname,
         autocommit=True
     )
+
     dbconn = connection.cursor()
 
     return dbconn
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return render_template("base.html")
 
 
-@app.route('/currentjoblist')
+@app.route('/currentjoblist', methods=['GET'])
 def current_job_list():
-    # cursor = getCursor()
-    # cursor.execute(
-    #     """
-    #     SELECT customer.customer_id, customer.first_name, customer.family_name, job.job_date
-    #     FROM job
-    #     JOIN customer ON job.customer = customer.customer_id
-    #     WHERE job.completed = 0
-    #     ORDER BY customer.first_name;
-    #     """
-    # )
-    # data = cursor.fetchall()
-    #
-    # return render_template("currentjoblist.html",data=data)
-
-    """Current jobs route. Returns a paginated list of current jobs."""
     cursor = getCursor()
 
     page = int(request.args.get('page', 1))
@@ -63,26 +48,37 @@ def current_job_list():
     cursor.execute("SELECT COUNT(*) FROM job WHERE completed=0;")
     total = cursor.fetchone()[0]
 
-    if selection and search:
+    if selection == 'Customer ID':
+        selection_query = 'customer.customer_id'
+    elif selection == 'Customer Name':
+        selection_query = "CONCAT(customer.first_name, ' ', customer.family_name)"
+    elif selection == 'Job Date':
+        selection_query = 'job.job_date'
+    else:
+        selection_query = None
+
+    if selection_query:
+        search = f'%{search}%'
         cursor.execute(
             f"SELECT customer.customer_id, customer.first_name, customer.family_name, job.job_date "
-            f"FROM job INNER JOIN customer ON job.customer = customer.customer_id "
-            f"WHERE job.customer LIKE %s AND job.completed=0 LIMIT %s OFFSET %s;",
-            (f'%{search}%', per_page, offset)
+            f"FROM job "
+            f"INNER JOIN customer ON job.customer = customer.customer_id "
+            f"WHERE {selection_query} LIKE %s "
+            f"AND job.completed=0 "
+            f"LIMIT %s OFFSET %s;",
+            (search, per_page, offset)
         )
     else:
         cursor.execute(
             "SELECT customer.customer_id, customer.first_name, customer.family_name, job.job_date "
-            "FROM job INNER JOIN customer ON job.customer = customer.customer_id "
-            "WHERE job.completed=0 LIMIT %s OFFSET %s;",
+            "FROM job "
+            "INNER JOIN customer ON job.customer = customer.customer_id "
+            "WHERE job.completed=0 "
+            "LIMIT %s OFFSET %s;",
             (per_page, offset)
         )
 
     data = cursor.fetchall()
-
-    no_result = False
-    if len(data) == 0:
-        no_result = True
 
     pagination = Pagination(page=page, total=total, per_page=per_page, record_name=' current jobs')
 
@@ -90,11 +86,9 @@ def current_job_list():
         'currentjoblist.html',
         data=data,
         pagination=pagination,
-        no_result=no_result,
         selection=selection,
         search=search
     )
-
 
 
 if __name__ == '__main__':
